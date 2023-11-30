2a3e9e0f74d9aeaa1b7e8063b66ec2c93ca035ea
yanghoonkim <ad26kt@gmail.com>
2023-11-30 02:21:25 +0000

    Update for nia

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-python tools/test.py nia/rtmdet_l_nia.py work_dirs/rtmdet_l_nia/epoch_5.pth
+python tools/test.py nia/rtmdet_l_nia.py work_dirs/rtmdet_l_nia/epoch_5.pth --cfg-options test_evaluator.classwise=True
