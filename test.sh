796e5da180e5dfd45b7898f11523c596bfbdca84
yanghoonkim <ad26kt@gmail.com>
2024-02-07 06:32:06 +0000

    Update for nia

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
-python tools/test.py nia/rtmdet_l_nia.py work_dirs/rtmdet_l_nia/epoch_5.pth \
---cfg-options test_evaluator.classwise=True \
---json-prefix results \
-# --show-dir show_results
-# 추론 결과를 저장된 이미지로 확인하고 싶을 경우 활성화 위의 주석 활성화
+python tools/test.py nia/rtmdet_l_nia.py work_dirs/finetuned/epoch_5.pth \
+#--cfg-options test_evaluator.classwise=True \
+# evaluation시 classwise AP를 확인하고 싶은 경우 활성화
+#--json-prefix results \
+# json-prefix: the prefix of the output json file without perform evaluation 
+#--show-dir show_results
+# show-dir: 추론 결과를 저장된 이미지로 확인하고 싶을 경우 활성화
